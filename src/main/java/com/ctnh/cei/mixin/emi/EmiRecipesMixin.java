8608acf2d07bbbcdd0f91df3a49a7c9a562c0143
1luik <8632787+n1luik@user.noreply.gitee.com>
2026-09-05 22:28:52 +0800

    emi多线程

--- src/main/java/com/ctnh/cei/mixin/emi/EmiRecipesMixin.java
+++ src/main/java/com/ctnh/cei/mixin/emi/EmiRecipesMixin.java
@@ -7,12 +7,17 @@ import dev.emi.emi.api.recipe.EmiRecipeManager;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.registry.EmiRecipes;
 import dev.emi.emi.runtime.EmiLog;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Mixin(value = EmiRecipes.class, remap = false)
 public class EmiRecipesMixin {
@@ -38,5 +43,9 @@ public class EmiRecipesMixin {
         long start = System.currentTimeMillis();
         manager = new FastRecipeManager(categories, workstations, recipes);
         EmiLog.info("Fast baked recipes in " + (System.currentTimeMillis() - start) + "ms");
+    }
+    @Redirect(method = "<clinit>", at = @At(value = "FIELD", target = "Ldev/emi/emi/registry/EmiRecipes;workstations:Ljava/util/Map;", opcode = Opcodes.PUTSTATIC))
+    private static void cei$newHashMap(Map<EmiRecipeCategory, List<EmiIngredient>> value) {
+        workstations = new ConcurrentHashMap<>(value);
     }
 }
